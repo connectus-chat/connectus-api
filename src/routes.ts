1837c2eb052e7d7548147d524d8deb053abda400
Gabriel Lima <lima.gabrieldsantos@gmail.com>
2024-01-02 12:46:55 -0400

    fix: add message routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import {GroupRoutes} from './api/routes/groups'
+import {MessageRoutes} from './api/routes/messages'
 import {UserRoutes} from './api/routes/users'
 
 export const router = express.Router()
 
 router.use(UserRoutes)
 router.use(GroupRoutes)
+router.use(MessageRoutes)
